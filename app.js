18e8c5f262d3c9e9b83cad7696b01e141de09c6f
Fikri Fadillah <fkrfdllh@gmail.com>
2023-02-05 17:34:23 +0700

    add router for api

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 const express = require("express");
 
-const router = require("./routes/web");
+const routerWeb = require("./routes/web");
+const routerApi = require("./routes/api");
 
 const app = express();
 const port = process.env.APP_PORT;
@@ -11,6 +12,7 @@ const port = process.env.APP_PORT;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-router(app);
+routerApi(app);
+routerWeb(app);
 
 app.listen(port, () => console.log(`SepiMU running on port ${port}!`));
