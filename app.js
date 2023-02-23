fe21675d5ffea6080214e1021fa3bc3e738e0ff7
Fikri Fadillah <fkrfdllh@gmail.com>
2023-02-23 19:58:35 +0700

    set view engine to ejs, add static folder

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const bodyParser = require("body-parser");
 const express = require("express");
+const appRootPath = require("app-root-path");
 
 const routerWeb = require("./routes/web");
 const routerApi = require("./routes/api");
@@ -9,8 +10,11 @@ const routerApi = require("./routes/api");
 const app = express();
 const port = process.env.APP_PORT;
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(`${appRootPath}/public`));
+
+app.set("view engine", "ejs");
 
 routerApi(app);
 routerWeb(app);
