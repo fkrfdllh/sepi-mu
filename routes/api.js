9368dee5f5fd8d0c5377fc62cdce0eb3aa258915
Fikri Fadillah <fkrfdllh@gmail.com>
2023-02-23 20:01:34 +0700

    set endpoint to handle api

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,16 @@
 require("express-group-routes");
 
 const syncDataController = require("../app/controllers/api/syncDataController");
+const calculateController = require("../app/controllers/api/calculateController");
 
 module.exports = (route) => {
 	route.group("/api", (route) => {
 		route.post("/sync-data", (req, res) => {
 			syncDataController(req, res);
+		});
+
+		route.get("/calculate", (req, res) => {
+			calculateController(req, res);
 		});
 	});
 };
